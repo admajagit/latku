5f34f3488fb807b469fb206cc0346cbbde13a833
admajagit <128940180+admajagit@users.noreply.github.com>
2024-03-19 09:41:08 +0700

    wkwkwkowkowwowok

--- poke.go
+++ poke.go
@@ -2,11 +2,6 @@ package latihanmodule
 
 import "fmt"
 
-
-func Jeneng(nama string)  {
-	fmt.Println("hallo", nama)
-}
-
-func Umur(um int)  {
-	fmt.Println("umur saya", um)
+func Masalah()  {
+	fmt.Println("bismillah bisa")
 }
